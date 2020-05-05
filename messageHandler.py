5b36ea76867a212ae5dcba57cf824b105a5e04c5
fargothcor <levchenya.roman@gmail.com>
2020-05-05 13:00:16 +0500

    added the "/" commands

--- messageHandler.py
+++ messageHandler.py
@@ -3,23 +3,27 @@ import Gandalf.DFapi as DFapi
 import os
 import importlib
 from Gandalf.command_class import command_list
+userInCommands = {}
 
 
 def load_modules():
-   files = os.listdir("Gandalf/commands")
-   modules = filter(lambda x: x.endswith('.py'), files)
-   for m in modules:
-       importlib.import_module("Gandalf.commands." + m[0:-3])
+    files = os.listdir("Gandalf/commands")
+    modules = filter(lambda x: x.endswith('.py'), files)
+    for m in modules:
+        importlib.import_module("Gandalf.commands." + m[0:-3])
 
 
 def create_answer(data):
     userID = data['user_id']
     text = data['body']
-    if text[0] == '/':
+    if userID in userInCommands:
+        message = userInCommands[userID].process(text)
+    elif text[0] == '/':
         load_modules()
         for c in command_list:
             if text == c.key:
-                message = c.process(text)
+                userInCommands[userID] = c
+                message = c.desription
     else:
         message = DFapi.get_smart_response(text)
     sendMessage(userID, message)
