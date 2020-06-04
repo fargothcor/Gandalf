cd96c4dd6f2c005d7bcfb76db6367d435067c38d
fargothcor <levchenya.roman@gmail.com>
2020-06-04 15:22:41 +0500

    api version fixes

--- apiVk.py
+++ apiVk.py
@@ -1,8 +1,9 @@
 import vk
 from Gandalf.config import TOKEN
+from random import randint
 session = vk.Session()
-api = vk.API(session, v=5.50)
+api = vk.API(session, v=5.107)
 
 
 def sendMessage(user_id, message, attachment=""):
-    api.messages.send(access_token=TOKEN, user_id=str(user_id), message=message, attachment=attachment)
+    api.messages.send(access_token=TOKEN, user_id=str(user_id), message=message, attachment=attachment, random_id=randint(0, 99999999))
