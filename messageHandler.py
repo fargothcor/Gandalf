5771068b4dbf35de2e679a1f1b2a586ff6931e30
fargothcor <levchenya.roman@gmail.com>
2020-06-06 23:46:47 +0500

    api versions fixes

--- messageHandler.py
+++ messageHandler.py
@@ -14,8 +14,8 @@ def load_modules():
 
 
 def create_answer(data):
-    userID = data['user_id']
-    text = data['body']
+    userID = data['peer_id']
+    text = data['text']
     if userID in userInCommands:
         message = userInCommands[userID].process(text)
         userInCommands.pop(userID)
