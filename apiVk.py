9ffb10390415d3da0e57490c96c93dbba38975f9
fargothcor <levchenya.roman@gmail.com>
2020-06-06 23:53:23 +0500

    api versions fixes

--- apiVk.py
+++ apiVk.py
@@ -8,7 +8,7 @@ api = vk.API(session, v=5.107)
 def sendMessage(user_id, message, attachment=""):
     api.messages.send(
         access_token=TOKEN,
-        user_id=str(user_id),
+        peer_id=str(user_id),
         message=message,
         attachment=attachment,
         random_id=randint(0, 99999999)
